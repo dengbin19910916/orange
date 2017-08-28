f0a212baabe5ee305a067116b1e59a33007ef817
dengb <dengbin19910916@163.com>
2017-08-29 00:53:25 +0800

    add config

--- product/src/main/java/com/geek/orange/product/ProductApplication.java
+++ product/src/main/java/com/geek/orange/product/ProductApplication.java
@@ -1,15 +1,26 @@
 package com.geek.orange.product;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
+
+	@Value("${from}")
+	private String from;
+
+	@GetMapping("/from")
+	public String hello() {
+	    return "hello, " + from + "!";
+    }
 }
